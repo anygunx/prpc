87a5cfddbe1ced6a84ac7d5a3b4ac8fde2427bcd
Lucifer <Hotlala8088@gmail.com>
2017-07-14 15:43:32 +0800

    change scanner

--- scanner/main.cpp
+++ scanner/main.cpp
@@ -1,20 +1,12 @@
 #include "config.h"
 #include "scanner.h"
 
-
-/** arpcc编译器入口. */
-int main( int argc, char *argv[] )
-{
-
-	try 
-	{
+int main( int argc, char *argv[] ){
+	try {
 		Scanner::Ref().Run(argc,argv);	
 	}
-	catch(...) 
-	{
+	catch(...) {
 		return 1;
 	}
-
-	// 编译源文件
 	return 0;
 }
